c9027a1c2d3cfed616624c7b96c113bee0423d90
dskiciak <dskiciak@gmail.com>
2021-03-20 17:55:22 +0100

    add  simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import express, { Express } from "express";
+import { PORT } from "./constans";
+
+class App {
+    public server: Express = express();
+
+    constructor() {}
+
+    run() {
+        this.server.listen(PORT, () => {
+            console.log(`Server starting on port: ${PORT}`);
+        });
+    }
+}
+
+const service = new App();
+service.run();
