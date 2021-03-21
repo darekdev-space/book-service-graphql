1bdb37ce2a50ae88aa9bb7a7d7b56494652b8293
dskiciak <dskiciak@gmail.com>
2021-03-21 20:48:02 +0100

    add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
+import 'reflect-metadata';
+import {ApolloServer} from 'apollo-server-express';
 import express, {Application} from 'express';
+import {buildSchema} from 'type-graphql';
 
 import {PORT} from './constans';
+import HelloResolver from './resolvers/hello';
 
 class App {
     public server: Application = express();
 
-    // constructor() {}
+    constructor() {
+        this.addMiddleware();
+    }
+
+    async createApolloServer(): Promise<ApolloServer> {
+        return new ApolloServer({
+            schema: await buildSchema({
+                resolvers: [HelloResolver],
+                validate: false,
+            }),
+        });
+    }
+
+    async addMiddleware(): Promise<void> {
+        const apolloServer = await this.createApolloServer();
+        apolloServer.applyMiddleware({app: this.server});
+    }
 
     run() {
         this.server.listen(PORT, () => {
